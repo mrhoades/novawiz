661e6278f18d00436a440cae6cc9a16c738e5520
Tim Miller <tim.miller.0@gmail.com>
2012-08-15 10:58:23 -0700

    add skeleton package files

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='novawiz',
+    version='0.0.1',
+    packages=['novawiz',],
+    license='Do What The Fuck You Want To Public License',
+    long_description=open('README.md').read(),
+)
