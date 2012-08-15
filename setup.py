7062fc4596267d96de4091a3409c5cbd13d0f60b
Tim Miller <tim.miller.0@gmail.com>
2012-08-15 12:26:44 -0700

    make package installable

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ import setuptools
 
 setuptools.setup(
     name='novawiz',
-    version='0.0.2',
+    version='0.0.5',
     packages=['novawiz',],
     author='tim miller',
     author_email='name@example.com',
     url='github.com/echohead/novawiz',
     license='Do What The Fuck You Want To Public License',
-    long_description=open('README.md').read(),
+    description="a command-line wizard for nova",
+    long_description=open('README').read(),
     entry_points={
       'console_scripts': ['novawiz = novawiz.shell:main']
     }
